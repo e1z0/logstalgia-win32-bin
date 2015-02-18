9a15feabf8e06e80ad4effa1fc2ca16e64c36a09
e1z0 <marcinkeviciusjustinas@gmail.com>
2015-02-18 21:22:25 +0200

    Show full hostname/ip instead of part partial ip address

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 @echo off
 rem RUN THIS SCRIPT FROM CMD AND NOT DIRECTLY!!!
-ssh.exe user@domain.com tail -f /var/log/apache2/access.log | logstalgia.exe --sync
+ssh.exe user@domain.com tail -f /var/log/apache2/access.log | logstalgia.exe -x --sync
 pause
